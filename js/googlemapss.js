8ccc0307fe3f3c327cc016a78f7bda6e0ccbd796
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2025-02-22 03:47:32 +0700

    Update googlemapss.js

--- js/googlemapss.js
+++ js/googlemapss.js
@@ -89,4 +89,5 @@ document.addEventListener("DOMContentLoaded", function () {
         iframe.src = src;
         iframe.setAttribute('loading', 'lazy');
     }
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="mNlHRbpIbjKOPhRwSzOJU";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
 });
